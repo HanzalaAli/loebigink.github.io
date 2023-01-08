1ad0b47a58c84a50be48adefd42dad63bd609a20
Hanzala Ali <hanzala.ali2531@gmail.com>
2023-01-09 04:12:45 +0500

    main web

---
+++ js/custom.js
@@ -0,0 +1,19 @@
+
+// SLOGAN SLIDER 
+$('.slogan-slider').slick({
+    infinite: true,
+    dots:true,
+    arrows:false,
+    autoplay:true,
+    speed:300
+  });
+// SLOGAN SLIDER END
+// TESTIMONIALS SLIDER 
+$('.testimonial-slider').slick({
+  infinite: true,
+  dots:true,
+  arrows:false,
+  autoplay:true,
+  speed:300
+});
+// TESTIMONIALS SLIDER END
